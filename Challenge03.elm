41e658c51ed6fcd1dabd8b33079477f25af776de
Gabriel Pickl <peacemotion@gmail.com>
2017-12-14 14:56:24 +0100

    Fixed recursion problems.

--- Challenge03.elm
+++ Challenge03.elm
@@ -2,12 +2,17 @@ module Challenge03 exposing (..)
 
 import List.Extra as LExtra
 import Array
-import Stream exposing (Stream)
+import Lazy.List as LazyList exposing (LazyList, (+++))
+import Lazy
+import Dict exposing (Dict)
 
 
-spiralDistance : Int -> Int
-spiralDistance value =
-    0
+-- Challenge Part 1 --
+
+
+challenge03a : Int -> Int
+challenge03a value =
+    (positionDistance value) + (baseDistance value)
 
 
 sideDistances : Int -> List Int
@@ -58,9 +63,13 @@ positionDistance value =
             |> Maybe.withDefault 0
 
 
-distance : Int -> Int
-distance value =
-    (positionDistance value) + (baseDistance value)
+
+-- Challenge Part 2 --
+
+
+challenge03b : Int -> Maybe Int
+challenge03b value =
+    findFirst (\a -> a > value) additionValues
 
 
 type alias Pos =
@@ -72,31 +81,97 @@ add ( x1, y1 ) ( x2, y2 ) =
     ( x1 + x2, y1 + y2 )
 
 
-moves : Stream Pos
+{-| Actually not necessary because part 1 is solved differently
+-}
+hamiltonDistance : Pos -> Int
+hamiltonDistance ( x, y ) =
+    x + y
+
+
+moves : LazyList Pos
 moves =
     let
+        takeOnly n v =
+            LazyList.repeat v |> LazyList.take n
+
         circle n =
-            List.repeat (2 * n - 1) ( 1, 0 )
-                ++ List.repeat (2 * n - 1) ( 0, 1 )
-                ++ List.repeat (2 * n) ( -1, 0 )
-                ++ List.repeat (2 * n) ( 0, -1 )
+            takeOnly (2 * n - 1) ( 1, 0 )
+                +++ takeOnly (2 * n - 1) ( 0, 1 )
+                +++ takeOnly (2 * n) ( -1, 0 )
+                +++ takeOnly (2 * n) ( 0, -1 )
     in
-        Stream.create (\a -> Stream.continue (a + 1)) 1
-            |> Stream.concatMap (circle >> Stream.fromList)
+        LazyList.numbers
+            |> LazyList.map circle
+            |> LazyList.flatten
 
 
+takeNth : Int -> LazyList a -> Maybe a
+takeNth n list =
+    case LazyList.headAndTail list of
+        Just ( x, xs ) ->
+            if n <= 0 then
+                Just x
+            else
+                takeNth (n - 1) xs
 
--- scanl : (a -> b -> b) -> b -> LazyList a -> LazyList b
--- scanl f start list =
---     case LazyList.headAndTail list of
---         Just ( head, tail ) ->
---             let
---                 newPos =
---                     f head start
---             in
---                 Lazy.lazy (\() -> LazyList.Cons start (scanl f newPos tail))
---         Nothing ->
---             Lazy.lazy (\() -> LazyList.Nil)
--- positions : LazyList Pos
--- positions =
---     scanl add ( 0, 0 ) moves
+        Nothing ->
+            Nothing
+
+
+findFirst : (a -> Bool) -> LazyList a -> Maybe a
+findFirst f list =
+    case LazyList.headAndTail list of
+        Just ( x, xs ) ->
+            if f x then
+                Just x
+            else
+                findFirst f xs
+
+        Nothing ->
+            Nothing
+
+
+scanl : (a -> b -> b) -> b -> LazyList a -> LazyList b
+scanl f start list =
+    case LazyList.headAndTail list of
+        Just ( head, tail ) ->
+            let
+                newPos =
+                    f head start
+            in
+                Lazy.lazy (\() -> LazyList.Cons start (scanl f newPos tail))
+
+        Nothing ->
+            Lazy.lazy (\() -> LazyList.Nil)
+
+
+positions : LazyList Pos
+positions =
+    scanl add ( 0, 0 ) moves
+
+
+modifyMap : Pos -> Dict Pos Int -> ( Int, Dict Pos Int )
+modifyMap pos map =
+    let
+        modifiers =
+            LazyList.fromList [ -1, 0, 1 ]
+
+        positionModifiers =
+            LazyList.product2 modifiers modifiers
+
+        positions =
+            LazyList.map (add pos) positionModifiers
+
+        values =
+            LazyList.map (\pos -> Dict.get pos map) positions
+
+        newValue =
+            LazyList.filterMap identity values |> LazyList.sum
+    in
+        ( newValue, Dict.insert pos newValue map )
+
+
+additionValues : LazyList Int
+additionValues =
+    scanl (\pos ( _, map ) -> modifyMap pos map) ( 0, Dict.singleton ( 0, 0 ) 1 ) positions
+        |> LazyList.map Tuple.first
